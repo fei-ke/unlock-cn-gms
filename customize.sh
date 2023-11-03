d57628747e069b6613bb3df925754abc8b140291
fei-ke <jinyang656@gmail.com>
2023-11-03 13:41:47 +0800

    Fix KernelSU

--- customize.sh
+++ customize.sh
@@ -11,15 +11,15 @@ elif [ -e /product/etc/permissions/cn.google.services.xml ]; then
 elif [ -e /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml ]; then
     origin=/my_bigball/etc/permissions/oplus_google_cn_gms_features.xml
 else
-    exit 0
+    abort "File not found!"
 fi
 
-if [[ $origin == *system* ]]; then
-    target=$MODPATH$origin
-elif [[ $origin == *my_bigball* ]]; then
+if [[ $origin == *my_bigball* ]]; then
     target=$MODPATH/oplus_google_cn_gms_features.xml
     echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
     # echo 'sleep 60; umount /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/service.sh
+elif [[ $origin == *system* || $KSU ]]; then
+    target=$MODPATH$origin    
 else
     target=$MODPATH/system$origin
 fi
@@ -28,3 +28,4 @@ mkdir -p $(dirname $target)
 cp -f $origin $target
 sed -i '/cn.google.services/d' $target
 sed -i '/services_updater/d' $target
+ui_print "modify $origin"
