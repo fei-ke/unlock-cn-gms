721f837d46b6bab06080536fae3b9372e89066ad
Nagisa_Cheese <mew@mevv.moe>
2026-03-30 22:17:39 +0800

    Support multiple permission files and add /odm partition (Xiaomi 8e5 devices)

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # Unlock CN GMS
-The GMS is restricted in China on some devices, because of a configuration file located in `/system/etc/permissions/services.cn.google.xml`.
-by this restriction, you can not enable `Google Location History` and the `Google Map Timeline` function can not use either.
+The GMS is restricted in China on some devices, because of configuration files that declare `cn.google.services` feature.
+By this restriction, you can not enable `Google Location History` and the `Google Map Timeline` function can not use either.
 
-This module removes the restriction by replacing this configuration file.
+This module removes the restriction by replacing these configuration files.
+
+Supported file locations:
+- `/system/etc/permissions/services.cn.google.xml`
+- `/system/etc/permissions/com.oppo.features.cn_google.xml`
+- `/vendor/etc/permissions/services.cn.google.xml`
+- `/product/etc/permissions/services.cn.google.xml`
+- `/product/etc/permissions/cn.google.services.xml`
+- `/product/etc/sysconfig/cn_feature.xml`
+- `/odm/etc/permissions/com.gnss.bds_preference.xml` (Xiaomi 8e5 等机型)
+- `/my_bigball/etc/permissions/oplus_google_cn_gms_features.xml`
+- `/my_product/etc/permissions/oplus_google_cn_gms_features.xml`
+- `/my_heytap/etc/permissions/my_heytap_cn_gms_features.xml`
 
 ---
 部分国行手机，或者本地化后的 ROM 中有内置 GMS ，但是某些功能无法使用，比如无法开启 `Google Location History` 服务，无法使用 `Google Map Timeline`，设备无法在 Web 版 Google play 中显示等等。
 
-该模块通过替换 `/system/etc/permissions/services.cn.google.xml` 文件, 具体为删除如下配置行：
+该模块通过查找并替换所有声明了 `cn.google.services` 的权限配置文件, 具体为删除如下配置行：
 
  ```xml
  <feature name="cn.google.services" />
  <feature name="com.google.android.feature.services_updater" />
  ``` 
  以此来实现去除国行 GMS 的限制。
+
+ 注意：部分设备上可能同时存在多个配置文件（如小米 8e5 同时在 `/odm` 和 `/product` 分区下各有一个），本模块会自动处理所有匹配的文件。对于独立挂载的分区（如 `/odm`、`/my_bigball` 等），通过 bind mount 方式替换。
 
  **注意**：为了开启 `Google Location History` 服务，你可能还需要配合其他模块   
  例如
--- customize.sh
+++ customize.sh
@@ -1,60 +1,68 @@
 #!/system/bin/sh
 
-# Function to find the first available XML permission file
-find_origin() {
-    for file in \
-        /system/etc/permissions/services.cn.google.xml \
-        /system/etc/permissions/com.oppo.features.cn_google.xml \
-        /vendor/etc/permissions/services.cn.google.xml \
-        /product/etc/permissions/services.cn.google.xml \
-        /product/etc/permissions/cn.google.services.xml \
-        /product/etc/sysconfig/cn_feature.xml \
-        /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml \
-        /my_product/etc/permissions/oplus_google_cn_gms_features.xml \
-        /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml; do
-        if [ -e "$file" ]; then
-            echo "$file"
-            return
+FILES="\
+/system/etc/permissions/services.cn.google.xml
+/system/etc/permissions/com.oppo.features.cn_google.xml
+/vendor/etc/permissions/services.cn.google.xml
+/product/etc/permissions/services.cn.google.xml
+/product/etc/permissions/cn.google.services.xml
+/product/etc/sysconfig/cn_feature.xml
+/odm/etc/permissions/com.gnss.bds_preference.xml
+/my_bigball/etc/permissions/oplus_google_cn_gms_features.xml
+/my_product/etc/permissions/oplus_google_cn_gms_features.xml
+/my_heytap/etc/permissions/my_heytap_cn_gms_features.xml"
+
+found=false
+need_bind_mount=false
+
+for origin in $FILES; do
+    [ ! -e "$origin" ] && continue
+    found=true
+
+    if [[ $origin == *my_bigball* ]]; then
+        target=$MODPATH/oplus_google_cn_gms_features.xml
+        echo "mount -o ro,bind \${0%/*}/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml" >> $MODPATH/post-fs-data.sh
+        need_bind_mount=true
+    elif [[ $origin == *my_product* ]]; then
+        target=$MODPATH/oplus_google_cn_gms_features.xml
+        echo "mount -o ro,bind \${0%/*}/oplus_google_cn_gms_features.xml /my_product/etc/permissions/oplus_google_cn_gms_features.xml" >> $MODPATH/post-fs-data.sh
+        need_bind_mount=true
+    elif [[ $origin == *my_heytap* ]]; then
+        target=$MODPATH/my_heytap_cn_gms_features.xml
+        echo "mount -o ro,bind \${0%/*}/my_heytap_cn_gms_features.xml /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml" >> $MODPATH/post-fs-data.sh
+        need_bind_mount=true
+        if [[ -e /my_heytap/etc/permissions/my_heytap_cn_features.xml ]]; then
+            echo "mount -o ro,bind \${0%/*}/my_heytap_cn_features.xml /my_heytap/etc/permissions/my_heytap_cn_features.xml" >> $MODPATH/post-fs-data.sh
+            heytap_cn_features_orgin=/my_heytap/etc/permissions/my_heytap_cn_features.xml
+            heytap_cn_features_target=$MODPATH/my_heytap_cn_features.xml
         fi
-    done
-    abort "No suitable permission file found!"
-}
-
-origin=$(find_origin)
-
-if [[ $origin == *my_bigball* ]]; then
-    target=$MODPATH/oplus_google_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-    # echo 'sleep 60; umount /my_bigball/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/service.sh
-elif [[ $origin == *my_product* ]]; then
-    target=$MODPATH/oplus_google_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/oplus_google_cn_gms_features.xml /my_product/etc/permissions/oplus_google_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-elif [[ $origin == *my_heytap* ]]; then
-    target=$MODPATH/my_heytap_cn_gms_features.xml
-    echo -e '#!/system/bin/sh\nmount -o ro,bind ${0%/*}/my_heytap_cn_gms_features.xml /my_heytap/etc/permissions/my_heytap_cn_gms_features.xml' > $MODPATH/post-fs-data.sh
-    if [[ -e /my_heytap/etc/permissions/my_heytap_cn_features.xml ]]; then
-        echo -e '\nmount -o ro,bind ${0%/*}/my_heytap_cn_features.xml /my_heytap/etc/permissions/my_heytap_cn_features.xml' >> $MODPATH/post-fs-data.sh
-        heytap_cn_features_orgin=/my_heytap/etc/permissions/my_heytap_cn_features.xml
-        heytap_cn_features_target=$MODPATH/my_heytap_cn_features.xml
+    elif [[ $origin == /odm/* ]]; then
+        target=$MODPATH/$(basename $origin)
+        echo "mount -o ro,bind \${0%/*}/$(basename $origin) $origin" >> $MODPATH/post-fs-data.sh
+        need_bind_mount=true
+    elif [[ $origin == *system* ]]; then
+        target=$MODPATH$origin
+    else
+        target=$MODPATH/system$origin
     fi
-elif [[ $origin == *system* ]]; then
-    target=$MODPATH$origin
-else
-    target=$MODPATH/system$origin
+
+    mkdir -p $(dirname $target)
+    cp -f $origin $target
+    sed -i '/cn.google.services/d' $target
+    sed -i '/services_updater/d' $target
+    ui_print "modify $origin"
+done
+
+if $need_bind_mount; then
+    sed -i '1i#!/system/bin/sh' $MODPATH/post-fs-data.sh
 fi
-
-DIR_OF_TARGET=$(dirname $target)
-mkdir -p $DIR_OF_TARGET
-cp -f $origin $target
-sed -i '/cn.google.services/d' $target
-sed -i '/services_updater/d' $target
-
-ui_print "modify $origin"
 
 if [[ -e "$heytap_cn_features_orgin" ]]; then
-mkdir -p $(dirname $heytap_cn_features_target)
-cp -f $heytap_cn_features_orgin $heytap_cn_features_target
-sed -i '/cn.google.services/d' $heytap_cn_features_target
-sed -i '/services_updater/d' $heytap_cn_features_target
-ui_print "modify $heytap_cn_features_orgin"
+    mkdir -p $(dirname $heytap_cn_features_target)
+    cp -f $heytap_cn_features_orgin $heytap_cn_features_target
+    sed -i '/cn.google.services/d' $heytap_cn_features_target
+    sed -i '/services_updater/d' $heytap_cn_features_target
+    ui_print "modify $heytap_cn_features_orgin"
 fi
+
+$found || abort "No suitable permission file found!"
